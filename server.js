744e555c1ab43f680c0ad12e773027e1fca42a7b
pramodkumar07web <elearning012345@gmail.com>
2025-06-27 15:09:10 +0530

    Connected the frontend to the backend

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,8 +12,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, './src')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './src/index.html'));
+});
+
+
 // Email Sending Endpoint
-app.post("/send-email", async (req, res) => {
+app.post("/api/send-email", async (req, res) => {
   const { name, email, message } = req.body;
 
   if (!name || !email || !message) {
